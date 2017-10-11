32dc5321e6990ca361e3b4dd4b8e7c530ae586fe
Jyoti Vashist <jyotivashisth95@gmail.com>
2017-10-11 16:16:52 +0530

    Adding objects to an array

--- ObjnArr.js
+++ ObjnArr.js
@@ -1,4 +1,5 @@
 var Lis={};
+var arr=[];//created an array
 var stdin=process.openStdin();
 var flag=0;
 var count=0;
@@ -8,11 +9,59 @@ stdin.addListener("data",function(d){
     console.log('count is: ',count);
     
     var addData=d.toString().trim();
+    
     console.log('entered data is:',addData);
     
     //add multiple properties to the list
-    Lis[addData]='xy';
+    //create an array with the desired valuesa and then add it to the list
+    //var obj={name:addData};
+    //var eval(d.toString().trim();)=[];
+    //eval(d.toString().trim();).push('xy');
+    //Lis.push(eval(d.toString().trim();));
+    //Lis[addData]='xyz';
     
+    /*edit-2 
+    var client='xyz';
+    var subscriber='{'+client+'}';
+    var ObjToAdd={};
+    ObjToAdd[addData]='client';
+    
+    Lis[ObjToAdd]='';
+    
+    */
+    
+    //Add an object to the array
+    
+    var pat=/pub/;
+    var Ob={};
+    if(pat.test(addData)){ //if a publisher
+        Ob[addData]='';    
+    }
+    else{
+        //check which publisher to subscriber to 
+        //var subs=findsubs(addData);
+        var pa=/23/;
+        var sub;
+        if(pa.test(addData)){
+            sub='subscriber1';
+        }
+        else{
+           sub='subscriber2'; 
+        }
+        
+        Ob[sub]=addData
+    }
+    
+    
+    arr.push(Ob);
+    
+    
+    for(var v in arr){
+        console.log(arr[v]);
+    }
+    
+    
+    /*edit-1
     var keys=Object.keys(Lis).length;
     console.log('length is: ',keys);
     count++;
@@ -25,7 +74,9 @@ stdin.addListener("data",function(d){
     
     //view the value of the properties
     for(var v in Lis){
-        console.log(Lis[v]);
+        for(var p in v){
+        console.log(v[p]);
+        }
     }
-    
+    */
 });
