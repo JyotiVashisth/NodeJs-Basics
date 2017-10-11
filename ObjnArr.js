5523d2895b2c166203b29872391a5b8775311088
Jyoti Vashist <jyotivashisth95@gmail.com>
2017-10-11 12:26:10 +0530

    add and view the object properties

--- ObjnArr.js
+++ ObjnArr.js
@@ -1,32 +1,26 @@
 var Lis={};
 var stdin=process.openStdin();
 var flag=0;
-var count=5;
+var count=0;
 stdin.addListener("data",function(d){
     //console.log("entered word:"+d.toString().trim()); //to string b'coz the returned data is an object and has linfeed embedded at the end of it.
-    // keep dding ht ewords to the list
+    
+    console.log('count is: ',count);
+    
     var addData=d.toString().trim();
     console.log('entered data is:',addData);
     
-    if(flag==0){
-        Lis.data=new Array(7);
-        Lis.data.push(addData);
-    }
-    Lis.data.push(addData);
+    //add multiple properties to the list
+    Lis[addData]='xy';
+    
+    var keys=Object.keys(Lis).length;
+    console.log('length is: ',keys);
     count++;
     
-    /*
-    Lis.addData=d.toString(); //here addData becomes a property!!
-    */
-    
-    var i=0;
-    //display the list
+//view the items added
+    console.log('and the items are:');
     for(var v in Lis){
-        
-       console.log('items are :',v);
-        i++;
-        
+        console.log(v);
     }
-    i=0;
     
 });
